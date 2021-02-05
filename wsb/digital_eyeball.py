c3639e41af88fd0e4caf2d24e6c5bb6a4843555a
parab\rah <rah@paraboliclabs.com>
2021-02-05 17:47:24 -0500

    31st

--- wsb/digital_eyeball.py
+++ wsb/digital_eyeball.py
@@ -11,26 +11,29 @@ k?
 https://praw.readthedocs.io/en/latest/getting_started/quick_start.html
 """
 
-reddit = praw.Reddit(
-	client_id="CLIENT_ID",
-	client_secret="CLIENT_SECRET",
-	password="PASSWORD",
-	user_agent="USERAGENT",
-	username="USERNAME",
-)
+# Use praw.ini file
+# goto https://www.reddit.com/prefs/apps and make an app and update the praw.ini with the information.
+# [WallStreetBets-TextToStonks]
+# client_id=xxxxxxxxxxxxxx
+# client_secret=xxxxxxxxxxxxxxxxxxxxxxxxxxxx
+# password=xxxxxxxxxxxxxx
+# username=xxxxxxxxxxxxxx
+
+reddit = praw.Reddit("WallStreetBets-TextToStonks",
+                     user_agent="Win:WallStreetBets-TextToStonks:0.0 (by /u/DrPixelBits)")
 
 url = "https://www.reddit.com/r/funny/comments/3g1jfi/buttons/"
 submission = reddit.submission(url=url)  # submission url
 submission = reddit.submission(id="3g1jfi")  # submission’s ID
 
 try:
-	for top_level_comment in submission.comments:
-		print(top_level_comment.body)
+    for top_level_comment in submission.comments:
+        print(top_level_comment.body)
 except AttributeError as ae:
-	print(ae)
+    print(ae)
 
 
 for top_level_comment in submission.comments:
-	if isinstance(top_level_comment, MoreComments):
-		continue
-	print(top_level_comment.body)
+    if isinstance(top_level_comment, MoreComments):
+        continue
+    print(top_level_comment.body)
